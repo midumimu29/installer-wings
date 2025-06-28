8dadd88869c1bd25fa79351b342c3f6d85cb6471
Midu <midumimu29@gmail.com>
2025-06-28 13:42:46 +0000

    Tambahkan skrip instalasi Wings

---
+++ wings.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Midu Dev
+
+echo " Installing Pterodactyl Panel Menggunakan Docker..."
+
+# Step 1: Buat struktur directory
+mkdir -p pterodactyl/wings
+cd pterodactyl/wings || exit
+
+# Step 2: Buat file docker-compose.yml
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+services:
+  wings:
+    image: ghcr.io/pterodactyl/wings:v1.6.1
+    restart: always
+    networks:
+      - wings0
+    ports:
+      - "8080:8080"
+      - "2022:2022"
+      - "443:443"
+    tty: true
+    environment:
+      TZ: "UTC"
+      WINGS_UID: 988
+      WINGS_GID: 988
+      WINGS_USERNAME: pterodactyl
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - /var/lib/docker/containers/:/var/lib/docker/containers/
+      - /etc/pterodactyl/:/etc/pterodactyl/
+      - /var/lib/pterodactyl/:/var/lib/pterodactyl/
+      - /var/log/pterodactyl/:/var/log/pterodactyl/
+      - /tmp/pterodactyl/:/tmp/pterodactyl/
+      - /etc/ssl/certs:/etc/ssl/certs:ro
+      # Optional: If upgrading from older daemon versions
+      # - /srv/daemon-data/:/srv/daemon-data/
+      # Optional: Required for SSL if using Let's Encrypt
+      # - /etc/letsencrypt/:/etc/letsencrypt/
+
+networks:
+  wings0:
+    name: wings0
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 172.21.0.0/16
+    driver_opts:
+      com.docker.network.bridge.name: wings0
+
+EOF
+
+# Step 3: Jalankan Docker kontainer
+docker-compose up -d
+
+echo "wings done.."
